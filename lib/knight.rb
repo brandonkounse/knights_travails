f0f9e4d9452659650bf9a88eb0a43bd476c9361d
Brandon <kounse@gmail.com>
2023-04-02 11:21:12 -0500

    Updated method name move_valid? to destination_valid?

--- lib/knight.rb
+++ lib/knight.rb
@@ -6,13 +6,15 @@ class Knight
 
   def initialize; end
 
-  # method to move knight forward 2 and 1 to the side
   def move(pos_from = [0, 0], pos_to = [0, 0])
-    # break apart both indices and add or subtract movement to determine new space
-    move_valid?(pos_to)
+    # makes sure destination is within board space
+    destination_valid?(pos_to)
+
+    return if pos_from == pos_to
+
   end
 
-  def move_valid?(pos_to = [])
+  def destination_valid?(pos_to = [])
     raise ArgumentError, "#{pos_to} is invalid square" if pos_to.any? { |ele| ele.negative? || ele > 7 }
 
     true
