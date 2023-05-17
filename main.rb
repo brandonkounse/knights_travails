1e63a7490c866419d29bea06a10d5d4fb7880caa
Brandon <kounse@gmail.com>
2023-05-17 10:10:27 -0500

    added multiple test cases to make sure it returned the correct results

--- main.rb
+++ main.rb
@@ -3,4 +3,6 @@
 require './lib/chess'
 
 chess = Chess.new
-chess.knight.move([0, 0], [7, 3])
+p chess.knight.move([0, 0], [7, 3])
+p chess.knight.move([5, 4], [7, 1])
+p chess.knight.move([1, 4], [7, 7])
