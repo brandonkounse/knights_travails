35370f3107191502f7a230a2f0ce8f8980571467
Brandon <kounse@gmail.com>
2023-05-17 10:11:47 -0500

    Created move public method that utilizes a BFS algorithm to determine the shortest path. Private methods for evaluating if the move is legal inside the board, calculating the new position, and the logic for the BFS algorithm

--- lib/knight.rb
+++ lib/knight.rb
@@ -9,4 +9,44 @@ class Knight
   def initialize
     @movement = [[-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1]]
   end
+
+  def move(current_pos, destination_pos)
+    bfs(current_pos, destination_pos)
+  end
+
+  private
+
+  def bfs(start_pos, destination_pos)
+    queue = [[start_pos]]
+    visited = [start_pos]
+
+    until queue.empty?
+      current_path = queue.shift
+      current_pos = current_path.last
+
+      return current_path if current_pos == destination_pos
+
+      calculate_new_pos(current_pos).each do |new_pos|
+        next if visited.include?(new_pos)
+
+        new_path = current_path + [new_pos]
+        queue << new_path
+        visited << new_pos
+      end
+    end
+  end
+
+  def calculate_new_pos(current_pos, possible_moves = [])
+    @movement.each do |mv|
+      new_pos = [(current_pos[0] + mv[0]), (current_pos[1] + mv[1])]
+      possible_moves << new_pos if inside_board?(new_pos)
+    end
+    possible_moves
+  end
+
+  def inside_board?(board_space)
+    return false if board_space.any? { |space| space.negative? || space > 7 }
+
+    true
+  end
 end
